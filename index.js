7155773b05efe2b12f22b073d7b34b827cb8852b
Ann <cchouaa@connect.ust.hk>
2022-01-02 11:39:51 +0800

    [database] mongodb connection setup completed

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
+const express = require('express');
 const mongoose = require('mongoose');
-
-const { MONGODB } = require('./config');
+const app = express();
+const { MONGO_URL } = require('./config');
 
 const PORT = process.env.PORT || 4000;
 
-mongoose
-    .connect(MONGODB, { useNewUrlParser: true,  useUnifiedTopology: true })
-    .then(()=> {
-        console.log('MongoDB Connected!');
-        return server.listen({ port: PORT });
-    })
-    .then(res=>{
-        console.log(`Server listening at ${res.url}`);
-    })
-    .catch(err => {
-       console.error(err); 
+mongoose.connect(MONGO_URL, { useNewUrlParser: true,  useUnifiedTopology: true })
+const db = mongoose.connection;
+db.on('error', (error) => {
+    console.error(error)
+});
+db.once('open', () => {
+    console.log('MongoDB connected!')
 });
