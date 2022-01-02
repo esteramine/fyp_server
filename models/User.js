d4508bd20ed7aee04bebf296f151275334e0dbd3
Ann <cchouaa@connect.ust.hk>
2022-01-02 17:27:05 +0800

    [database] user schema added

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+    itsc: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    // deviceId: {
+    //     type: String,
+    //     required: true,
+    // }
+    createdAt: {
+        type: String,
+        required: true
+    }
+    // studentId: {
+    //     type: String,
+    //     required: true,
+    //     unique: true, 
+    // },
+    // school: {
+    //     type: String,
+    //     required: true,
+    // }
+
+});
+
+module.exports = model('User', userSchema);
