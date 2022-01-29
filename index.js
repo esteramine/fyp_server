a1ecf97e444329b59815ecfab03fd0429bfd5abc
Ann <cchouaa@connect.ust.hk>
2022-01-29 16:17:18 +0800

    [server] apollo server set up with mongoose

--- index.js
+++ index.js
@@ -1,82 +1,20 @@
-const app = require('./app');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const { MONGO_URL } = require('./config');
-const User = require('./models/User');
-const { validateRegisterInput, validateLoginInput } = require('./utils/validators');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
-const PORT = process.env.PORT || 4000;
-
-mongoose.connect(MONGO_URL, { useNewUrlParser: true,  useUnifiedTopology: true })
-const db = mongoose.connection;
-db.on('error', (error) => {
-    console.error(error);
-});
-db.once('open', () => {
-    console.log('MongoDB connected!');
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
 });
 
-app.get('/', (req, res) => {
-    res.send('Server Successfully Setup!');
-});
-
-app.post('/register', (req, res) => {
-    const { itsc, password, confirmedPassword } = req.body;
-    const { errors, valid } = validateRegisterInput(itsc, password, confirmedPassword);
-    if (!valid) {
-        res.send(errors);
-    }
-    else {
-        User.findOne({ itsc }, (err, user) => {
-            if (user) {
-                errors.itsc = 'This ITSC has been registerd before.';
-                res.send(errors);
-            }
-            else {
-                bcrypt.hash(password, 13, (err, hash) => {
-                    const newUser = new User({
-                        itsc, 
-                        password: hash,
-                        createdAt: new Date().toISOString()
-                    });
-                    User.create(newUser);
-                    res.send('Successfully Registered!');
-                });  
-            }
-        });    
-    }
-});
-
-app.post('/login', (req, res) => {
-    const { itsc, password } = req.body;
-    const { errors, valid } = validateLoginInput(itsc, password);
-    if (!valid) {
-        res.send(errors);
-    }
-    else {
-        User.findOne({ itsc }, (err, user) => {
-            if (!user) {
-                errors.itsc = 'User does not exist!';
-                res.send(errors);
-            }
-            else {
-                bcrypt.compare(password, user.password, (err, result) => {
-                    if (!result) {
-                        errors.password = 'Password incorrect!';
-                        res.send(errors);
-                    }
-                    else {
-                        res.send('Successfully Login!');
-                    }
-                });
-            }
-
-        });
-    }
-
-});
-
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
-});
+mongoose.connect(MONGO_URL, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDB connected!');
+        return server.listen({ port: 5000 });
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    });
