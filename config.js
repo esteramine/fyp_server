6ace24ebb36ef028bc5482638753783b9470493a
Ann <cchouaa@connect.ust.hk>
2022-02-11 15:33:28 +0800

    add config file

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URL: 'mongodb+srv://chihanchou:chihanchou@cluster0.r32ha.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    SECRET_KEY: 'This is the encryption key.'
+}
