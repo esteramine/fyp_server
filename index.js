1a462fa4376c69c14ee57a62fadc454fafbe1975
Ann <cchouaa@connect.ust.hk>
2022-02-11 15:25:18 +0800

    changed server port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { MONGO_URL } = require('./config');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
+const PORT = process.env.PORT || 5000;
+
 const server = new ApolloServer({
     typeDefs, 
     resolvers,
@@ -14,7 +16,7 @@ const server = new ApolloServer({
 mongoose.connect(MONGO_URL, { useNewUrlParser: true })
     .then(() => {
         console.log('MongoDB connected!');
-        return server.listen({ port: 5000 });
+        return server.listen({ port: PORT });
     })
     .then(res => {
         console.log(`Server running at ${res.url}`);
