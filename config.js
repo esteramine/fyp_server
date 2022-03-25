25d1577ed4e135e09fd84250dd7b16b7130dfc3f
Ann <cchouaa@connect.ust.hk>
2022-03-26 01:13:51 +0800

    config aws s3 instance

--- config.js
+++ config.js
@@ -1,5 +1,18 @@
+require('dotenv').config();
+
 module.exports = {
     // MONGO_URL: 'mongodb+srv://chihanchou:chihanchou@cluster0.r32ha.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     MONGO_URL: 'mongodb+srv://chihanchou:chihanchou@cluster0.m5wtt.mongodb.net/testDB?retryWrites=true&w=majority',
-    SECRET_KEY: 'This is the encryption key.'
+    SECRET_KEY: 'This is the encryption key.',
+    s3: {
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+        },
+        region: process.env.AWS_S3_REGION,
+        params: {
+            ACL: 'public-read',
+            Bucket: process.env.AWS_S3_BUCKET,
+        },
+    },
 }
